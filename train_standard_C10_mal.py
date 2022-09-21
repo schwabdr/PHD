0a69384505f933b8182a1ce79d5e3ed892b6bf0a
David Schwab <david.r.schwab@gmail.com>
2022-09-21 21:06:54 +0000

    complete train_standard_C10_mal.py

--- train_standard_C10_mal.py
+++ train_standard_C10_mal.py
@@ -39,31 +39,44 @@ from models.resnet_new import ResNet18
 
 args = config.Configuration().getArgs()
 
-resnet = ResNet18(10)
-
-full_dataset = datasets.ImageFolder('./data/malimg_paper_dataset_imgs')
-print(full_dataset)
-
-loader = DataLoader(full_dataset,batch_size=256, num_workers=0, shuffle=False)
-mean = 0.
-std = 0.
-nb_samples = 0.
-for data, _ in loader:
-    print(data.shape)
-    batch_samples = data.size(0)
-    data = data.view(batch_samples, data.size(1), -1)
-    mean += data.mean(2).sum(0)
-    std += data.std(2).sum(0)
-    nb_samples += batch_samples
-    break
-
-mean /= nb_samples
-std /= nb_samples
-
+args.epochs = 25
 
 '''
+Function to find some information about our dataset.
+'''
+def calcStdMean():
+    transform = transforms.Compose([
+        # you can add other transformations in this list
+        transforms.ToTensor()
+    ])
 
-print(f"Preparing to train a ResNet18 Model on CIFAR10 dataset ...")
+    full_dataset = datasets.ImageFolder('./data/malimg_paper_dataset_imgs', transform=transform)
+    print(full_dataset)
+
+    loader = DataLoader(full_dataset,batch_size=1, num_workers=0, shuffle=False)
+    mean = 0.
+    std = 0.
+    nb_samples = 0.
+    for data, _ in loader:
+        print(data.shape)
+        batch_samples = data.size(0)
+        data = data.view(batch_samples, data.size(1), -1)
+        mean += data.mean(2).sum(0)
+        std += data.std(2).sum(0)
+        nb_samples += batch_samples
+        break
+
+    mean /= nb_samples
+    std /= nb_samples
+
+    #mean: tensor([0.4454, 0.4454, 0.4454])
+    #std: tensor([0.3122, 0.3122, 0.3122])
+
+
+
+
+
+print(f"Preparing to train a ResNet18 Model on MalImg dataset ...")
 
 print(64*'=')
 utils.print_cuda_info()
@@ -86,27 +99,27 @@ def adjust_learning_rate(optimizer, epoch):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
-def eval_test(model, device, test_loader):
+def eval_model(model, device, loader, type="Test"):
     model.eval()
-    test_loss = 0
+    loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target in test_loader:
+        for data, target in loader:
             data, target = data.to(device), target.to(device)
             
             output = model(data)
-            #test_loss += F.cross_entropy(output, target, size_average=False).item() #old, size_average is deprecated
-            test_loss += F.cross_entropy(output, target, reduction='sum').item()
+            loss += F.cross_entropy(output, target, reduction='sum').item()
             pred = output.max(1, keepdim=True)[1]
             correct += pred.eq(target.view_as(pred)).sum().item()
         
-    test_loss /= len(test_loader.dataset)
-    print('Test: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+    loss /= len(loader.dataset)
+    print('{}: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(type,
+        loss, correct, len(loader.dataset),
+        100. * correct / len(loader.dataset)))
 
-    test_accuracy = correct / len(test_loader.dataset)
-    return test_loss, test_accuracy
+    accuracy = correct / len(loader.dataset)
+    return loss, accuracy
+
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
@@ -118,7 +131,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
         loss = F.cross_entropy(model(data), target)
         loss.backward() #backprop the gradients
         optimizer.step() #update the weights
-
+        
         # print progress
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
@@ -134,16 +147,18 @@ def main():
 
     print(f"device: {device}") #sanity check - using GPU
 
-    #normalization stats
-    stats = ((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)) #mean and stdev
-    #https://www.kaggle.com/code/kmldas/cifar10-resnet-90-accuracy-less-than-5-min/notebook
-    #stats = ((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    #normalization stats - calculated above
+    #mean: tensor([0.4454, 0.4454, 0.4454])
+    #std: tensor([0.3122, 0.3122, 0.3122])
 
+    stats = ((0.4454, 0.4454, 0.4454), (0.3122, 0.3122, 0.3122)) #mean and stdev
+    
     
     # setup data loader
     trans_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4, padding_mode='reflect'),
-        transforms.RandomHorizontalFlip(),
+        #transforms.RandomCrop(32, padding=4, padding_mode='reflect'),
+        #transforms.RandomHorizontalFlip(),
+        transforms.Resize((224,224)),
         transforms.ToTensor(),
         transforms.Normalize(*stats, inplace=False) #original code was True here from MIAT - not sure why, just making a note
     ])
@@ -152,30 +167,28 @@ def main():
         transforms.ToTensor(),
         transforms.Normalize(*stats)
     ])
-    #now I need to pull in the data and create the dataloaders
-    # this link states that we do indeed move the data out of the range [0,1] - so I guess this is correct.
-    # https://www.kaggle.com/code/fanbyprinciple/cifar10-explanation-with-pytorch
-    # min: -1.9259666204452515
-    # max: 2.130864143371582
 
-    trainset = data.data_dataset(img_path=args.nat_img_train, clean_label_path=args.nat_label_train, transform=trans_train)
-    testset = data.data_dataset(img_path=args.nat_img_test, clean_label_path=args.nat_label_test, transform=trans_test)
+    full_dataset = datasets.ImageFolder('./data/malimg_paper_dataset_imgs', transform=trans_train)
+    print(f"len(full_dataset): {len(full_dataset)}")
+    #split dataset 90% train, 10% test
+    subsets_dataset = torch.utils.data.random_split(full_dataset, [8405, 934], generator=torch.Generator().manual_seed(42)) #still the best seed ever
 
-    #create data loaders
-    train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, drop_last=False,
-                                               shuffle=True, num_workers=4, pin_memory=True)
-    test_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, drop_last=False, shuffle=False,
-                                              num_workers=4, pin_memory=True)
+    #data loaders
+    train_loader = DataLoader(subsets_dataset[0],batch_size=16, num_workers=8, shuffle=True)
+    test_loader = DataLoader(subsets_dataset[1],batch_size=16, num_workers=8, shuffle=False)
+
+    resnet = ResNet18(25) #25 classes
 
     model = resnet.to(device)
     model = torch.nn.DataParallel(model)
 
-    summary(model, input_size=(args.batch_size,3,32,32), verbose=2)
+    #summary(model, input_size=(args.batch_size,3,32,32), verbose=2)
     
     cudnn.benchmark = True
     optimizer = optim.Adagrad(model.parameters(), lr=args.lr)
 
     #now we begin the actual training
+    model_name = 'resnet-mal-std'
     for epoch in range(1, args.epochs + 1):
         # adjust learning rate for SGD
         adjust_learning_rate(optimizer, epoch) #inefficient to call this every epoch?
@@ -184,18 +197,17 @@ def main():
         train(args, model, device, train_loader, optimizer, epoch)
 
         print(64*'=')
-        # eval_train(model, device, train_loader)
-        test_loss, test_accuracy = eval_test(model, device, test_loader)
+        train_loss, train_acc = eval_model(model, device, train_loader, type="Train")
+        test_loss, test_accuracy = eval_model(model, device, test_loader, type="Test")
         print(64*'=')
     
-    model_name = 'resnet-new-100'
-    print(f"Saving resnet model '{model_name}' ...")
-    utils.save_model(model, model_name)
-    print("Save Complete. Exiting ...")
+        name = model_name + str(epoch)
+        print(f"Saving resnet model '{name}' ...")
+        utils.save_model(model, name)
+        print("Save Complete. Exiting ...")
 
     print(f"Training Complete. Exiting ...")
 
 if __name__ == '__main__':
     main()
 
-'''
