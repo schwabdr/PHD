af81a973775289255f2faa0b145c178ba661dc0c
David Schwab <david-schwab@utc.edu>
2022-10-22 10:34:40 -0400

    Finally done with CIFAR10! Added some code to display sample images

--- eval_tests_01.py
+++ eval_tests_01.py
@@ -17,7 +17,7 @@ import torchvision.models as models
 import numpy as np
 
 from utils import config
-from utils import data
+from utils.data import data_dataset
 from projected_gradient_descent import projected_gradient_descent as pgd
 from models.resnet_new import ResNet18
 
@@ -32,14 +32,49 @@ classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship'
 
 args = config.Configuration().getArgs()
 stats = config.Configuration().getNormStats()
+#using 512 here for generating and displaying adv examples.
+args.batch_size=512#2048-2048 was original for the test  #trying this - this works with the data parallel - GPU util ~95%, memory 10800/11019 MB each GPU
+f = False
+# these are the indices randomly selected from the first batch - I printed them to save for all future tests.
+a =  [365, 234, 109, 63, 90, 94, 265, 238, 198, 129, 35, 56, 372, 478, 427, 329, 165, 27, 300, 22, 462, 486, 31, 187, 43, 330, 179, 263, 6, 328, 384, 97, 285, 295, 92, 409, 272, 68, 139, 503, 354, 289, 65, 36, 117, 78, 442, 80, 406, 13, 348, 7, 11, 506, 459, 414, 436, 168, 284, 41, 370, 45, 293, 418, 480, 212, 341, 99, 135, 258, 89, 46, 323, 146, 134, 182, 339, 73, 487, 507, 26, 269, 143, 297, 349, 131, 227, 38, 237, 359, 426, 305, 77, 428, 24, 229, 358, 108, 244, 448, 9, 16, 120, 247, 152, 18, 140, 350, 389, 302, 55, 193, 124, 484, 88, 458, 403, 113, 47, 322, 125, 388, 489, 438, 421, 127, 312, 211, 412, 2, 277, 386, 53, 132, 192, 493, 225, 67, 275, 144, 268, 206, 173, 282, 317, 156, 153, 253, 242, 402, 391, 502, 315, 176, 326, 304, 171, 4, 149, 501, 380, 71, 385, 376, 208, 114, 452, 86, 419, 207, 76, 505, 290, 93, 495, 408, 130, 226, 202, 334, 169, 62, 363, 357, 101, 344, 210, 463, 316, 278, 460, 307, 72, 510, 228, 59, 195, 106, 451, 82, 17, 340, 85, 61, 471, 343, 183, 355, 447, 252, 240, 485, 469, 383, 443, 54, 369, 121, 267, 103, 218, 8, 74, 296, 175, 201, 281, 141, 496, 405, 167, 416, 338, 221, 83, 394, 236, 21, 461, 498, 137, 70, 230, 324, 214, 186, 194, 352, 320, 327, 353, 366, 450, 147, 399, 470, 142, 190, 29, 116, 245, 368, 177, 262, 455, 239, 204, 104, 488, 456, 429, 174, 356, 48, 360, 170, 500, 410, 128, 475, 199, 504, 69, 294, 508, 292, 28, 32, 115, 44, 306, 331, 361, 243, 422, 261, 390, 318, 260, 308, 345, 271, 248, 232, 23, 479, 122, 465, 332, 298, 446, 203, 259, 467, 424, 110, 241, 155, 33, 220, 401, 301, 51, 10, 437, 215, 466, 181, 362, 374, 273, 197, 396, 347, 415, 0, 377, 255, 160, 417, 439, 472, 381, 25, 433, 178, 509, 196, 373, 162, 79, 1, 34, 407, 145, 303, 299, 404, 57, 477, 159, 158, 42, 157, 336, 335, 432, 52, 185, 30, 393, 474, 314, 473, 440, 280, 20, 287, 12, 430, 367, 172, 453, 256, 497, 333, 161, 180, 398, 309, 392, 310, 37, 150, 75, 209, 337, 375, 188, 270, 191, 222, 274, 163, 400, 291, 420, 371, 283, 264, 148, 464, 387, 482, 279, 319, 233, 288, 257, 250, 235, 423, 483, 444, 254, 223, 395, 96, 39, 231, 166, 311, 313, 219, 382, 164, 249, 95, 205, 100, 492, 511, 251, 351, 91, 266, 87, 217, 84, 490, 449, 499, 107, 397, 14, 118, 123, 126, 216, 151, 58, 5, 105, 184, 468, 431, 138, 60, 378, 98, 64, 102, 481, 19, 454, 189, 154, 491, 411, 346, 434, 81, 476, 49, 286, 136, 325, 413, 379, 342, 15, 276, 112, 321, 111, 246, 224, 364, 66, 435, 213, 494, 441, 40, 50, 119, 445, 133, 3, 457, 425, 200]
+b =  [365, 234, 109, 63, 90, 94, 265, 238, 198, 129, 35, 56, 372, 478, 427, 329, 165, 27, 300, 22, 462, 486, 31, 187, 43, 330, 179, 263, 6, 328, 384, 97, 285, 295, 92, 409, 272, 68, 139, 503, 354, 289, 65, 36, 117, 78, 442, 80, 406, 13, 348, 7, 11, 506, 459, 414, 436, 168, 284, 41, 370, 45, 293, 418, 480, 212, 341, 99, 135, 258, 89, 46, 323, 146, 134, 182, 339, 73, 487, 507, 26, 269, 143, 297, 349, 131, 227, 38, 237, 359, 426, 305, 77, 428, 24, 229, 358, 108, 244, 448, 9, 16, 120, 247, 152, 18, 140, 350, 389, 302, 55, 193, 124, 484, 88, 458, 403, 113, 47, 322, 125, 388, 489, 438, 421, 127, 312, 211, 412, 2, 277, 386, 53, 132, 192, 493, 225, 67, 275, 144, 268, 206, 173, 282, 317, 156, 153, 253, 242, 402, 391, 502, 315, 176, 326, 304, 171, 4, 149, 501, 380, 71, 385, 376, 208, 114, 452, 86, 419, 207, 76, 505, 290, 93, 495, 408, 130, 226, 202, 334, 169, 62, 363, 357, 101, 344, 210, 463, 316, 278, 460, 307, 72, 510, 228, 59, 195, 106, 451, 82, 17, 340, 85, 61, 471, 343, 183, 355, 447, 252, 240, 485, 469, 383, 443, 54, 369, 121, 267, 103, 218, 8, 74, 296, 175, 201, 281, 141, 496, 405, 167, 416, 338, 221, 83, 394, 236, 21, 461, 498, 137, 70, 230, 324, 214, 186, 194, 352, 320, 327, 353, 366, 450, 147, 399, 470, 142, 190, 29, 116, 245, 368, 177, 262, 455, 239, 204, 104, 488, 456, 429, 174, 356, 48, 360, 170, 500, 410, 128, 475, 199, 504, 69, 294, 508, 292, 28, 32, 115, 44, 306, 331, 361, 243, 422, 261, 390, 318, 260, 308, 345, 271, 248, 232, 23, 479, 122, 465, 332, 298, 446, 203, 259, 467, 424, 110, 241, 155, 33, 220, 401, 301, 51, 10, 437, 215, 466, 181, 362, 374, 273, 197, 396, 347, 415, 0, 377, 255, 160, 417, 439, 472, 381, 25, 433, 178, 509, 196, 373, 162, 79, 1, 34, 407, 145, 303, 299, 404, 57, 477, 159, 158, 42, 157, 336, 335, 432, 52, 185, 30, 393, 474, 314, 473, 440, 280, 20, 287, 12, 430, 367, 172, 453, 256, 497, 333, 161, 180, 398, 309, 392, 310, 37, 150, 75, 209, 337, 375, 188, 270, 191, 222, 274, 163, 400, 291, 420, 371, 283, 264, 148, 464, 387, 482, 279, 319, 233, 288, 257, 250, 235, 423, 483, 444, 254, 223, 395, 96, 39, 231, 166, 311, 313, 219, 382, 164, 249, 95, 205, 100, 492, 511, 251, 351, 91, 266, 87, 217, 84, 490, 449, 499, 107, 397, 14, 118, 123, 126, 216, 151, 58, 5, 105, 184, 468, 431, 138, 60, 378, 98, 64, 102, 481, 19, 454, 189, 154, 491, 411, 346, 434, 81, 476, 49, 286, 136, 325, 413, 379, 342, 15, 276, 112, 321, 111, 246, 224, 364, 66, 435, 213, 494, 441, 40, 50, 119, 445, 133, 3, 457, 425, 200]
 
-args.batch_size=2048 #trying this - this works with the data parallel - GPU util ~95%, memory 10800/11019 MB each GPU
+def show_img_grid(rows, cols, x, x_adv, y, y_adv,fname=None):
+    fig, axes1 = plt.subplots(rows,cols,figsize=(25,25))
+    global f
+    global a
+    global b
+    if f:
+        lst = list(range(0, len(x)))
+        random.shuffle(lst)
+        a = lst.copy()
+        b = lst.copy()
+        print(f"Using these indices from first batch: {a}")
+        f = False
+    #print("min/max of numpy arrays: ", np.min(x), np.max(x)) #this was very close to 0 and 1
+    for j in range(rows):
+        for k in range(0,cols,2):
+            #get a random index
+            i = a.pop()
+            axes1[j][k].set_axis_off()
+            axes1[j][k+1].set_axis_off()
+            axes1[j][k].imshow(x[i],interpolation='nearest')
+            axes1[j][k].text(0,0,classes[y[i]]) # this gets the point accross but needs fixing.
+            axes1[j][k+1].imshow(x_adv[i], interpolation='nearest')
+            axes1[j][k+1].text(0,0,classes[y_adv[i]])
+    a = b.copy()
+
+    if fname is None:
+        plt.show()
+    else:
+        plt.savefig(fname, format='png')
+        plt.close()
 
 '''
 There's plenty of CPU RAM (I think) so I'm going to stack the NumPy data on the CPU, and send it all here
 number of rows and columns you want: pick 2x as many columns as you want images b/c you'll have clean / adv example alternating
 '''
-def show_img_grid(rows, cols, x, x_adv, y, y_adv):
+def show_img_grid_old(rows, cols, x, x_adv, y, y_adv):
     fig, axes1 = plt.subplots(rows,cols,figsize=(5,5))
     lst = list(range(0, len(x)))
     random.shuffle(lst)
@@ -150,7 +185,7 @@ def main():
         transforms.Normalize(*stats)
     ])
 
-    testset = data.data_dataset(img_path=args.nat_img_test, clean_label_path=args.nat_label_test, transform=trans_test)
+    testset = data_dataset(img_path=args.nat_img_test, clean_label_path=args.nat_label_test, transform=trans_test)
     test_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, drop_last=False, shuffle=False,
                                               num_workers=4, pin_memory=True)
     t_acc = []
@@ -169,8 +204,6 @@ def main():
     print(adv_accuracy)
     print("total loss:")
     print(test_loss)
-    
-    
     
     print(64*'=')
 
@@ -191,36 +224,92 @@ def main():
         for y in x:
             print(y)
 
-def print_examples():
-    print("Beginning Evaluation ...")
+#purpose of this function is simply to display adversarial examples
+def display_examples():
+    print("Beginning Image Display ...")
 
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
     device = torch.device("cuda" if use_cuda else "cpu")
 
-    name = 'resnet-new-100' #input("Name of model to load: ") #for now I'll hard code the only model I have trained
-    
-    model = ResNet18(10)
-   
-    model.load_state_dict(torch.load(os.path.join(args.SAVE_MODEL_PATH, name)))
-    model.to(device)
-    model = torch.nn.DataParallel(model).cuda() 
-    model.eval()
-
-    print(f"Model loaded: {name}")
+    model_names = ['resnet-new-100', 'resnet-new-100-MIAT-from-scratch', 'resnet-new-100-MIAT-0.1-from-scratch', 'resnet-new-100-MIAT-0.25-from-scratch']
     
     trans_test = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(*stats)
     ])
 
-    testset = data.data_dataset(img_path=args.nat_img_test, clean_label_path=args.nat_label_test, transform=trans_test)
+    testset = data_dataset(img_path=args.nat_img_test, clean_label_path=args.nat_label_test, transform=trans_test)
     test_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, drop_last=False, shuffle=False,
                                               num_workers=4, pin_memory=True)
     
+    for name in model_names:
+        #load model parameters for this test
+        model = ResNet18(10)
+        model.load_state_dict(torch.load(os.path.join(args.SAVE_MODEL_PATH, name)))
+        model.to(device)
+        model = torch.nn.DataParallel(model).cuda() 
+        model.eval()
+
+        print(f"Model loaded: {name}")
     
-   
+        '''these values will cause large perturbations, and nearly 100% misclassifications.
+        eps = 1.27
+        eps_iter = .05
+        nb_iter = 50
+        '''
+        
+        #this is our L_infty constraint 
+        eps_lst = [.025, .05, .075, .1, .125, .15, .175, .2, .25, .3, .4, .5, .75, 1.] #, 1.5, 2., 2.5]
+        #eps_lst = [.025, .05] # for quick test
+
+        for eps in eps_lst:
+            print(25*'=')
+            
+            eps_iter = .007
+            #nb_iter = round(eps/eps_iter) + 10
+            nb_iter = 100
+            print(f"Using PGD with eps: {eps}, eps_iter: {eps_iter}, nb_iter: {nb_iter}")
+            #with torch.no_grad():
+            for data, target in test_loader:
+                data, target = data.to(device), target.to(device)
+                output = model(data)
+                top2 = torch.topk(output, 2) # this is for a targeted attack
+                y_target = torch.select(top2.indices, 1, 1) #y_target is second most likely class
+                #data_adv = pgd(model, data, eps=eps, eps_iter=eps_iter, nb_iter=nb_iter, norm=np.inf, y=None, targeted=False) #NON_targeted attack
+                data_adv = pgd(model, data, eps=eps, eps_iter=eps_iter, nb_iter=nb_iter, norm=np.inf, y=y_target, targeted=True) #targeted attack
+
+                # here I'll have to look back at some old code for stacking np images together to show in a grid
+                #x_adv = data_adv.detach().cpu().numpy().transpose(0,2,3,1) #I'll use this later - gonna paste all the images together.
+                #output = model(data)
+                output_adv = model(data_adv)
+                pred = output.max(1, keepdim=True)[1] # we'll use this instead of true label -make sure it is misclassified
+                pred_adv = output_adv.max(1, keepdim=True)[1]
+                
+                x_test_clean = data.detach().cpu().numpy().transpose(0,2,3,1)
+                y_test_pred = pred.detach().cpu().numpy()
+                x_test_adv = data_adv.detach().cpu().numpy().transpose(0,2,3,1)
+                y_test_adv = pred_adv.detach().cpu().numpy()
+                # de normalize
+                x_test_clean = np.clip(((x_test_clean * stats[1]) + stats[0]),0,1.)
+                x_test_clean = (x_test_clean*255).astype(np.uint8)
+                x_test_adv = np.clip(((x_test_adv * stats[1]) + stats[0]),0,1.)
+                x_test_adv = (x_test_adv*255).astype(np.uint8)
+                
+                #x_test_adv is now [b][w][h][c] range of [0,255]
+                y_test_pred = y_test_pred.astype(np.uint8)
+                y_test_pred = y_test_pred.reshape((args.batch_size))
+                y_test_adv = y_test_adv.astype(np.uint8)
+                y_test_adv = y_test_adv.reshape((args.batch_size))
+
+                fname = os.path.join('./results/imgs/', str(eps) + "-" + name + "-PGD-CE-T.PNG") #T is for targeted attack, remove for non-targeted
+                show_img_grid(10,20, x_test_clean, x_test_adv, y_test_pred, y_test_adv, fname=fname)
+
+                break # do one batch for quick test
+            
+            
+            print(25*'=')
 
 if __name__ == '__main__':
     #main()
-    print_examples()
+    display_examples()
--- eval_tests_02.py
+++ eval_tests_02.py
@@ -62,12 +62,49 @@ import matplotlib.pyplot as plt
 
 import random
 
+classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
 args = config.Configuration().getArgs()
 stats = config.Configuration().getNormStats() #
 classes = config.Configuration().getClasses()
 
-args.batch_size = 512 #512 worked, so did wierd, this time 704 crashed
+args.batch_size = 512 #512 worked, 704 crashed
+f = False
+# these are the indices randomly selected from the first batch - I printed them to save for all future tests.
+a =  [365, 234, 109, 63, 90, 94, 265, 238, 198, 129, 35, 56, 372, 478, 427, 329, 165, 27, 300, 22, 462, 486, 31, 187, 43, 330, 179, 263, 6, 328, 384, 97, 285, 295, 92, 409, 272, 68, 139, 503, 354, 289, 65, 36, 117, 78, 442, 80, 406, 13, 348, 7, 11, 506, 459, 414, 436, 168, 284, 41, 370, 45, 293, 418, 480, 212, 341, 99, 135, 258, 89, 46, 323, 146, 134, 182, 339, 73, 487, 507, 26, 269, 143, 297, 349, 131, 227, 38, 237, 359, 426, 305, 77, 428, 24, 229, 358, 108, 244, 448, 9, 16, 120, 247, 152, 18, 140, 350, 389, 302, 55, 193, 124, 484, 88, 458, 403, 113, 47, 322, 125, 388, 489, 438, 421, 127, 312, 211, 412, 2, 277, 386, 53, 132, 192, 493, 225, 67, 275, 144, 268, 206, 173, 282, 317, 156, 153, 253, 242, 402, 391, 502, 315, 176, 326, 304, 171, 4, 149, 501, 380, 71, 385, 376, 208, 114, 452, 86, 419, 207, 76, 505, 290, 93, 495, 408, 130, 226, 202, 334, 169, 62, 363, 357, 101, 344, 210, 463, 316, 278, 460, 307, 72, 510, 228, 59, 195, 106, 451, 82, 17, 340, 85, 61, 471, 343, 183, 355, 447, 252, 240, 485, 469, 383, 443, 54, 369, 121, 267, 103, 218, 8, 74, 296, 175, 201, 281, 141, 496, 405, 167, 416, 338, 221, 83, 394, 236, 21, 461, 498, 137, 70, 230, 324, 214, 186, 194, 352, 320, 327, 353, 366, 450, 147, 399, 470, 142, 190, 29, 116, 245, 368, 177, 262, 455, 239, 204, 104, 488, 456, 429, 174, 356, 48, 360, 170, 500, 410, 128, 475, 199, 504, 69, 294, 508, 292, 28, 32, 115, 44, 306, 331, 361, 243, 422, 261, 390, 318, 260, 308, 345, 271, 248, 232, 23, 479, 122, 465, 332, 298, 446, 203, 259, 467, 424, 110, 241, 155, 33, 220, 401, 301, 51, 10, 437, 215, 466, 181, 362, 374, 273, 197, 396, 347, 415, 0, 377, 255, 160, 417, 439, 472, 381, 25, 433, 178, 509, 196, 373, 162, 79, 1, 34, 407, 145, 303, 299, 404, 57, 477, 159, 158, 42, 157, 336, 335, 432, 52, 185, 30, 393, 474, 314, 473, 440, 280, 20, 287, 12, 430, 367, 172, 453, 256, 497, 333, 161, 180, 398, 309, 392, 310, 37, 150, 75, 209, 337, 375, 188, 270, 191, 222, 274, 163, 400, 291, 420, 371, 283, 264, 148, 464, 387, 482, 279, 319, 233, 288, 257, 250, 235, 423, 483, 444, 254, 223, 395, 96, 39, 231, 166, 311, 313, 219, 382, 164, 249, 95, 205, 100, 492, 511, 251, 351, 91, 266, 87, 217, 84, 490, 449, 499, 107, 397, 14, 118, 123, 126, 216, 151, 58, 5, 105, 184, 468, 431, 138, 60, 378, 98, 64, 102, 481, 19, 454, 189, 154, 491, 411, 346, 434, 81, 476, 49, 286, 136, 325, 413, 379, 342, 15, 276, 112, 321, 111, 246, 224, 364, 66, 435, 213, 494, 441, 40, 50, 119, 445, 133, 3, 457, 425, 200]
+b =  [365, 234, 109, 63, 90, 94, 265, 238, 198, 129, 35, 56, 372, 478, 427, 329, 165, 27, 300, 22, 462, 486, 31, 187, 43, 330, 179, 263, 6, 328, 384, 97, 285, 295, 92, 409, 272, 68, 139, 503, 354, 289, 65, 36, 117, 78, 442, 80, 406, 13, 348, 7, 11, 506, 459, 414, 436, 168, 284, 41, 370, 45, 293, 418, 480, 212, 341, 99, 135, 258, 89, 46, 323, 146, 134, 182, 339, 73, 487, 507, 26, 269, 143, 297, 349, 131, 227, 38, 237, 359, 426, 305, 77, 428, 24, 229, 358, 108, 244, 448, 9, 16, 120, 247, 152, 18, 140, 350, 389, 302, 55, 193, 124, 484, 88, 458, 403, 113, 47, 322, 125, 388, 489, 438, 421, 127, 312, 211, 412, 2, 277, 386, 53, 132, 192, 493, 225, 67, 275, 144, 268, 206, 173, 282, 317, 156, 153, 253, 242, 402, 391, 502, 315, 176, 326, 304, 171, 4, 149, 501, 380, 71, 385, 376, 208, 114, 452, 86, 419, 207, 76, 505, 290, 93, 495, 408, 130, 226, 202, 334, 169, 62, 363, 357, 101, 344, 210, 463, 316, 278, 460, 307, 72, 510, 228, 59, 195, 106, 451, 82, 17, 340, 85, 61, 471, 343, 183, 355, 447, 252, 240, 485, 469, 383, 443, 54, 369, 121, 267, 103, 218, 8, 74, 296, 175, 201, 281, 141, 496, 405, 167, 416, 338, 221, 83, 394, 236, 21, 461, 498, 137, 70, 230, 324, 214, 186, 194, 352, 320, 327, 353, 366, 450, 147, 399, 470, 142, 190, 29, 116, 245, 368, 177, 262, 455, 239, 204, 104, 488, 456, 429, 174, 356, 48, 360, 170, 500, 410, 128, 475, 199, 504, 69, 294, 508, 292, 28, 32, 115, 44, 306, 331, 361, 243, 422, 261, 390, 318, 260, 308, 345, 271, 248, 232, 23, 479, 122, 465, 332, 298, 446, 203, 259, 467, 424, 110, 241, 155, 33, 220, 401, 301, 51, 10, 437, 215, 466, 181, 362, 374, 273, 197, 396, 347, 415, 0, 377, 255, 160, 417, 439, 472, 381, 25, 433, 178, 509, 196, 373, 162, 79, 1, 34, 407, 145, 303, 299, 404, 57, 477, 159, 158, 42, 157, 336, 335, 432, 52, 185, 30, 393, 474, 314, 473, 440, 280, 20, 287, 12, 430, 367, 172, 453, 256, 497, 333, 161, 180, 398, 309, 392, 310, 37, 150, 75, 209, 337, 375, 188, 270, 191, 222, 274, 163, 400, 291, 420, 371, 283, 264, 148, 464, 387, 482, 279, 319, 233, 288, 257, 250, 235, 423, 483, 444, 254, 223, 395, 96, 39, 231, 166, 311, 313, 219, 382, 164, 249, 95, 205, 100, 492, 511, 251, 351, 91, 266, 87, 217, 84, 490, 449, 499, 107, 397, 14, 118, 123, 126, 216, 151, 58, 5, 105, 184, 468, 431, 138, 60, 378, 98, 64, 102, 481, 19, 454, 189, 154, 491, 411, 346, 434, 81, 476, 49, 286, 136, 325, 413, 379, 342, 15, 276, 112, 321, 111, 246, 224, 364, 66, 435, 213, 494, 441, 40, 50, 119, 445, 133, 3, 457, 425, 200]
+
+def show_img_grid(rows, cols, x, x_adv, y, y_adv,fname=None):
+    fig, axes1 = plt.subplots(rows,cols,figsize=(25,25))
+    global f
+    global a
+    global b
+    if f:
+        lst = list(range(0, len(x)))
+        random.shuffle(lst)
+        a = lst.copy()
+        b = lst.copy()
+        print(f"Using these indices from first batch: {a}")
+        f = False
+    #print("min/max of numpy arrays: ", np.min(x), np.max(x)) #this was very close to 0 and 1
+    for j in range(rows):
+        for k in range(0,cols,2):
+            #get a random index
+            i = a.pop()
+            axes1[j][k].set_axis_off()
+            axes1[j][k+1].set_axis_off()
+            axes1[j][k].imshow(x[i],interpolation='nearest')
+            axes1[j][k].text(0,0,classes[y[i]]) # this gets the point accross but needs fixing.
+            axes1[j][k+1].imshow(x_adv[i], interpolation='nearest')
+            axes1[j][k+1].text(0,0,classes[y_adv[i]])
+    a = b.copy()
+
+    if fname is None:
+        plt.show()
+    else:
+        plt.savefig(fname, format='png')
+        plt.close()
+
 
 #####################################################
 #THIS CODE IS FROM train_MIAT_alpha.py
@@ -115,7 +152,7 @@ Name of this function is perhaps misleading as it gives TOTAL loss, not just the
 #               0        1         2        3         4        5
 #model_fns = [std_res, miat_res, local_n, global_n, local_a, global_a]
 def MI_loss2(model_fns, x_natural, y_true ,x_adv, alpha=5.0, lambd=0.1, iter=0):
-    model = model_fns[0] #[0] is std
+    model = model_fns[1] #[0] is std
     encoder = model_fns[0]
     local_n = model_fns[2]
     global_n = model_fns[3]
@@ -217,7 +254,7 @@ Name of this function is perhaps misleading as it gives TOTAL loss, not just the
 #               0        1         2        3         4        5
 #model_fns = [std_res, miat_res, local_n, global_n, local_a, global_a]
 def MI_loss(model_fns, x_natural, y_true ,x_adv, alpha=5.0, lambd=0.1, iter=0):
-    model = model_fns[1] #[0] is std
+    model = model_fns[1] #[0] is std [1] is MIAT
     
     local_n = model_fns[2]
     global_n = model_fns[3]
@@ -332,7 +369,7 @@ def fast_gradient_method(
             "eps must be greater than or equal to 0, got {} instead".format(eps)
         )
     
-    model_fn = model_fns[0] #[1] is miat [0] is std
+    model_fn = model_fns[1] #[1] is miat [0] is std
     
     if eps == 0:
         return x
@@ -451,7 +488,7 @@ def pgd(
     """
     #model_fns[0] is the target model
     #model_fns[1] is the model to construct adv samples with with (same as model_fns[0] for white-box attack)
-    model_fn = model_fns[0] # 0 is STD, 1 is MIAT
+    model_fn = model_fns[1] # 0 is STD, 1 is MIAT
     if norm == 1:
         raise NotImplementedError(
             "It's not clear that FGM is a good inner loop"
@@ -558,6 +595,130 @@ def pgd(
         assert np.all(asserts)
     return adv_x
 
+def display_examples():
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    
+    print(f"device: {device}") #sanity check - using GPU
+
+    model_names = ['resnet-new-100', 'resnet-new-100-MIAT-from-scratch', 'resnet-new-100-MIAT-0.1-from-scratch', 'resnet-new-100-MIAT-0.25-from-scratch']
+    estimator_suffixes = ['', '.1', '.25']
+
+    trans_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(*stats, inplace=False)
+    ])
+    print(f"Loading data ...")
+    #not going to augment the data for now - don't think I need to
+    testset=data_dataset(img_path=args.nat_img_test, clean_label_path=args.nat_label_test, transform=trans_test)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size, drop_last=False, shuffle=False, num_workers=4, pin_memory=True)
+    print("Data loaded!")
+
+    print(f"Loading Models to {device} ...")
+    #load resnet models
+    std_res = ResNet18(10)
+    miat_res = ResNet18(10)
+
+    # Estimator part 1: X or layer3 to H space
+    local_n = Estimator(args.va_hsize)
+    local_a = Estimator(args.va_hsize)
+
+    # estimator part 2: Z to H space
+    z_size = 10
+    global_n = MI1x1ConvNet(z_size, args.va_hsize)
+    global_a = MI1x1ConvNet(z_size, args.va_hsize)
+
+    std_res_name = 'resnet-new-100' 
+    miat_res_name = 'resnet-new-100-MIAT-0.25-from-scratch'
+    #miat_res_name = 'resnet-new-100-MIAT-from-scratch'
+    l_n = 'local_n.25'
+    g_n = 'global_n.25'
+    l_a = 'local_a.25'
+    g_a = 'global_a.25'
+
+    std_res.load_state_dict(torch.load(os.path.join(args.SAVE_MODEL_PATH, std_res_name)))
+    miat_res.load_state_dict(torch.load(os.path.join(args.SAVE_MODEL_PATH, miat_res_name)))
+    local_n.load_state_dict(torch.load(os.path.join(args.SAVE_MODEL_PATH, l_n)))
+    global_n.load_state_dict(torch.load(os.path.join(args.SAVE_MODEL_PATH, g_n)))
+    local_a.load_state_dict(torch.load(os.path.join(args.SAVE_MODEL_PATH, l_a)))
+    global_a.load_state_dict(torch.load(os.path.join(args.SAVE_MODEL_PATH, g_a)))
+    
+    print(f"Resnet Models Loaded: {std_res_name} {miat_res_name}")
+    print(f"Estimator Models Loaded: {l_n} {g_n} {l_a} {g_a}")
+    
+    std_res = torch.nn.DataParallel(std_res).cuda()
+    miat_res = torch.nn.DataParallel(miat_res).cuda()
+    local_n = torch.nn.DataParallel(local_n).cuda()
+    global_n = torch.nn.DataParallel(global_n).cuda()
+    local_a = torch.nn.DataParallel(local_a).cuda()
+    global_a = torch.nn.DataParallel(global_a).cuda()
+
+    std_res.eval()
+    miat_res.eval()
+    local_n.eval()
+    global_n.eval()
+    local_a.eval()
+    global_a.eval()
+
+    cudnn.benchmark = True
+    
+    print(f"Models Loaded!")
+    #               0        1         2        3         4        5
+    model_fns = [std_res, miat_res, local_n, global_n, local_a, global_a]
+    #model_fns = {'std': std_res, 'miat': miat_res, 'local_n':local_n, 'global_n': global_n, 'local_a': local_a, 'global_a': global_a}
+    loss_fn = MI_loss #MI_loss - MI-Craft, MI_loss2 - MI-Craft-Euc
+
+    target_model_fn = model_fns[1]
+    
+    eps_lst = [.025, .05, .075, .1, .125, .15, .175, .2, .25, .3, .4, .5, .75, 1.]
+    #eps_lst = [.025, .05, .075, .1]
+    #eps_lst = [.1,.15]
+
+    for eps in eps_lst:
+        
+        print(25*'=')
+        
+        eps_iter = .007
+        #eps_iter = .005
+        #nb_iter = round(eps/eps_iter) + 10
+        nb_iter = 100
+        print(f"Using PGD-MI-Craft with eps: {eps}, eps_iter: {eps_iter}, nb_iter: {nb_iter}")
+        #with torch.no_grad():
+        for x_natural, y_true in test_loader:
+            x_natural, y_true = x_natural.to(device), y_true.to(device)
+            
+            x_adv = pgd(model_fns, x_natural, eps=eps, eps_iter=eps_iter, nb_iter=nb_iter, norm=np.inf, y=None, y_true=y_true, targeted=False, rand_init=True, loss_fn=loss_fn)
+            
+            y_nat_pred = target_model_fn(x_natural)
+            y_adv_pred = target_model_fn(x_adv)
+            
+            pred = y_nat_pred.max(1, keepdim=True)[1]
+            pred_adv = y_adv_pred.max(1, keepdim=True)[1]
+
+            x_test_clean = x_natural.detach().cpu().numpy().transpose(0,2,3,1)
+            y_test_pred = pred.detach().cpu().numpy()
+            x_test_adv = x_adv.detach().cpu().numpy().transpose(0,2,3,1)
+            y_test_adv = pred_adv.detach().cpu().numpy()
+            # de normalize
+            x_test_clean = np.clip(((x_test_clean * stats[1]) + stats[0]),0,1.)
+            x_test_clean = (x_test_clean*255).astype(np.uint8)
+            x_test_adv = np.clip(((x_test_adv * stats[1]) + stats[0]),0,1.)
+            x_test_adv = (x_test_adv*255).astype(np.uint8)
+            
+            #x_test_adv is now [b][w][h][c] range of [0,255]
+            y_test_pred = y_test_pred.astype(np.uint8)
+            y_test_pred = y_test_pred.reshape((args.batch_size))
+            y_test_adv = y_test_adv.astype(np.uint8)
+            y_test_adv = y_test_adv.reshape((args.batch_size))
+            
+            fname = os.path.join('./results/imgs/', str(eps) + "-" + miat_res_name + "-MI-COS.PNG") #T is for targeted attack, remove for non-targeted
+            show_img_grid(10,20, x_test_clean, x_test_adv, y_test_pred, y_test_adv, fname=fname)       
+            
+            break # only printing from 1st batch of images    
+            
+                
+        print(25*'=')
+        
 #               0        1         2        3         4        5
 #model_fns = [std_res, miat_res, local_n, global_n, local_a, global_a] #now a dictionary but indexing should work.
 def eval_loss(model_fns, device, test_loader, loss_fn):
@@ -719,4 +880,5 @@ def main():
     
 
 if __name__ == '__main__':
-    main()
+    #main()
+    display_examples()
